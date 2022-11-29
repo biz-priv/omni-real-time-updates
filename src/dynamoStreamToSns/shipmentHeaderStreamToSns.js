2f32b256e0c433968ac795a227cc5d66783142b7
monowarbiz <iqbal.layek@bizcloudexperts.com>
2022-11-30 00:25:09 +0530

    sns topic and  queue

--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -3,34 +3,34 @@ const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 
 module.exports.handler = async (event, context, callback) => {
   console.info("event", event);
-  await snsPublish(event);
+  // await snsPublish(event);
 
   return {};
 };
 
-function snsPublish(event) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const TopicArn =
-        "arn:aws:sns:us-east-1:332281781429:omni-wt-rt-shipment-header-dev";
-      const params = {
-        Message: JSON.stringify(item),
-        TopicArn,
-        MessageAttributes: {
-          BillNo: {
-            DataType: "String",
-            StringValue: item.customer_id.toString(),
-          },
-          data: {},
-        },
-      };
-      //SNS service
-      const response = await sns.publish(params).promise();
-      console.log("SNS publish:::: ", response);
-      resolve(1);
-    } catch (error) {
-      console.log("SNSPublishError: ", error);
-      reject(error);
-    }
-  });
-}
+// function snsPublish(event) {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const TopicArn =
+//         "arn:aws:sns:us-east-1:332281781429:omni-wt-rt-shipment-header-dev";
+//       const params = {
+//         Message: JSON.stringify(item),
+//         TopicArn,
+//         MessageAttributes: {
+//           BillNo: {
+//             DataType: "String",
+//             StringValue: item.customer_id.toString(),
+//           },
+//           data: {},
+//         },
+//       };
+//       //SNS service
+//       const response = await sns.publish(params).promise();
+//       console.log("SNS publish:::: ", response);
+//       resolve(1);
+//     } catch (error) {
+//       console.log("SNSPublishError: ", error);
+//       reject(error);
+//     }
+//   });
+// }
