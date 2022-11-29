5d4e598c24b963147ac7b9162041cfe24ad0fd75
monowarbiz <iqbal.layek@bizcloudexperts.com>
2022-11-30 01:49:22 +0530

    sns topic and  queue

--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -3,34 +3,38 @@ const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 
 module.exports.handler = async (event, context, callback) => {
   console.info("event", JSON.stringify(event));
-  // await snsPublish(event);
+  console.log("BillNo", event.Records[0].dynamodb.NewImage.BillNo.S);
+  const NewImage = event.Records[0].dynamodb.NewImage;
+  // const BillNo = event.Records[0].dynamodb.NewImage.BillNo;
+  const BillNo = "22531";
+  
+  await snsPublish(NewImage, BillNo);
 
   return {};
 };
 
-// function snsPublish(event) {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const TopicArn =
-//         "arn:aws:sns:us-east-1:332281781429:omni-wt-rt-shipment-header-dev";
-//       const params = {
-//         Message: JSON.stringify(item),
-//         TopicArn,
-//         MessageAttributes: {
-//           BillNo: {
-//             DataType: "String",
-//             StringValue: item.customer_id.toString(),
-//           },
-//           data: {},
-//         },
-//       };
-//       //SNS service
-//       const response = await sns.publish(params).promise();
-//       console.log("SNS publish:::: ", response);
-//       resolve(1);
-//     } catch (error) {
-//       console.log("SNSPublishError: ", error);
-//       reject(error);
-//     }
-//   });
-// }
+function snsPublish(NewImage, BillNo) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const TopicArn =
+        "arn:aws:sns:us-east-1:332281781429:omni-wt-rt-shipment-header-dev";
+      const params = {
+        Message: JSON.stringify(NewImage),
+        TopicArn,
+        MessageAttributes: {
+          BillNo: {
+            DataType: "String",
+            StringValue: BillNo.toString(),
+          },
+        },
+      };
+      //SNS service
+      const response = await sns.publish(params).promise();
+      console.log("SNS publish:::: ", response);
+      resolve(1);
+    } catch (error) {
+      console.log("SNSPublishError: ", error);
+      reject(error);
+    }
+  });
+}
