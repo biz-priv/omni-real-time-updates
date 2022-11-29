5c853330dd54341d4df4db1656ee43f4f1245c37
monowarbiz <iqbal.layek@bizcloudexperts.com>
2022-11-30 00:21:50 +0530

    sns topic and  queue

--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -1,7 +1,36 @@
 const AWS = require("aws-sdk");
-const batch = new AWS.Batch({ apiVersion: "2016-08-10" });
+const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 
 module.exports.handler = async (event, context, callback) => {
   console.info("event", event);
+  await snsPublish(event);
+
   return {};
 };
+
+function snsPublish(event) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const TopicArn =
+        "arn:aws:sns:us-east-1:332281781429:omni-wt-rt-shipment-header-dev";
+      const params = {
+        Message: JSON.stringify(item),
+        TopicArn,
+        MessageAttributes: {
+          BillNo: {
+            DataType: "String",
+            StringValue: item.customer_id.toString(),
+          },
+          data: {},
+        },
+      };
+      //SNS service
+      const response = await sns.publish(params).promise();
+      console.log("SNS publish:::: ", response);
+      resolve(1);
+    } catch (error) {
+      console.log("SNSPublishError: ", error);
+      reject(error);
+    }
+  });
+}
