4168b689bf9d046c0b5988dd774fd90fb686398b
Abdul-rashed <122110062+Abdul-Rashed@users.noreply.github.com>
2023-02-03 22:42:28 +0530

    created lambda functions

---
+++ src/shared/models/consolStopHeadersRt.js
@@ -0,0 +1,33 @@
+const consolStopHeadersTableMapping = [
+    "PK_ConsolStopId", // Partition Key
+    "ConsolDeliveryAgent",
+    "ConsolPreventStatusUpdates",
+    "ConsolStopAddress1",
+    "ConsolStopAddress2",
+    "ConsolStopArea",
+    "ConsolStopCity",
+    "ConsolStopContact",
+    "ConsolStopCustomerVendorLookupData",
+    "ConsolStopCustomerVendorLookupState",
+    "ConsolStopDate",
+    "ConsolStopEmail",
+    "ConsolStopETADateTime",
+    "ConsolStopFax",
+    "ConsolStopName",
+    "ConsolStopNotes",
+    "ConsolStopNumber",
+    "ConsolStopPhone",
+    "ConsolStopPickupOrDelivery",
+    "ConsolStopTimeBegin",
+    "ConsolStopTimeEnd",
+    "ConsolStopZip",
+    "DMS_TS",
+    "FK_ConsolNo",
+    "FK_ConsolStopAirport",
+    "FK_ConsolStopCountry",
+    "FK_ConsolStopState",
+    "InsertedTimeStamp",
+    "transact_id",
+];
+
+module.exports = consolStopHeadersTableMapping;
---
+++ src/shared/models/consolStopItemsRt.js
@@ -0,0 +1,10 @@
+const consolStopItemsTableMapping = [
+    "FK_OrderNo",// Partition key
+    "FK_ConsolStopId", // Sort Key
+    "DMS_TS",	
+    "InsertedTimeStamp",	
+    "transact_id",
+  ];
+  
+  module.exports = consolStopItemsTableMapping;
+  
