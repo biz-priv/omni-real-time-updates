59ecd16564c3b4f2141a2dd7f732b48705dd636a
monowarbiz <iqbal.layek@bizcloudexperts.com>
2023-02-14 11:48:10 +0530

    dynamo stream remove event fix

--- src/dynamoStreamToSns/shipmentAparStreamToSns.js
+++ src/dynamoStreamToSns/shipmentAparStreamToSns.js
@@ -4,6 +4,9 @@ module.exports.handler = async (event, context, callback) => {
   console.info("event", JSON.stringify(event));
   const TopicArn = process.env.SNS_TOPIC_ARN;
   const tableName = process.env.DYNAMO_DB_TABLE;
+  if (event?.Records?.[0]?.eventName === "REMOVE") {
+    return "Dynamo REMOVE event";
+  }
   const FK_VendorId = event.Records[0].dynamodb.NewImage.FK_VendorId.S;
   console.log("FK_VendorId", FK_VendorId);
   const MessageAttributes = {
--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -3,9 +3,12 @@ const { snsPublish } = require("../shared/snsHelper");
 module.exports.handler = async (event, context, callback) => {
   console.info("event", JSON.stringify(event));
   const TopicArn = process.env.SNS_TOPIC_ARN;
+  const tableName = process.env.DYNAMO_DB_TABLE;
+  if (event?.Records?.[0]?.eventName === "REMOVE") {
+    return "Dynamo REMOVE event";
+  }
   const BillNo = event.Records[0].dynamodb.NewImage.BillNo.S;
   console.log("BillNo", BillNo);
-  const tableName = process.env.DYNAMO_DB_TABLE;
 
   const MessageAttributes = {
     BillNo: {
