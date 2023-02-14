e53c16da887eb56185dbaf118c5091d08ffbff9b
monowarbiz <iqbal.layek@bizcloudexperts.com>
2023-02-14 17:49:56 +0530

    dynamo stream remove event fix

--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -4,7 +4,11 @@ module.exports.handler = async (event, context, callback) => {
   console.info("event", JSON.stringify(event));
   const TopicArn = process.env.SNS_TOPIC_ARN;
   const tableName = process.env.DYNAMO_DB_TABLE;
-  if (event?.Records?.[0]?.eventName === "REMOVE") {
+  if (
+    event?.Records &&
+    event.Records.length > 0 &&
+    event.Records[0].eventName === "REMOVE"
+  ) {
     return "Dynamo REMOVE event";
   }
   const BillNo = event.Records[0].dynamodb.NewImage.BillNo.S;
