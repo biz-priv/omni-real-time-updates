dd46b6a29adc415c81261f1347d82439dce81b44
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2023-11-15 22:02:39 +0530

    turned on realtime replication for import tables

---
+++ shipment-header-full-load/models/shipmentAirImportMapping.js
@@ -0,0 +1,12 @@
+const shipmentAirImportMapping = [
+    "FK_OrderNo", // - {Primary Key}
+    "MAWB",
+    "CarrierId",
+    "Flight",
+    "AirlineName",
+    "InsertedTimeStamp", // value manually manually
+    "UUid", // value manually manually
+    "ProcessState" // value manually manually
+];
+
+module.exports = shipmentAirImportMapping;
---
+++ shipment-header-full-load/models/shipmentOceanImportMapping.js
@@ -0,0 +1,14 @@
+const shipmentOceanImportMapping = [
+    "FK_OrderNo", // - {Primary Key}
+    "MOBL",
+    "CarrierId",
+    "SteamShip",
+    "Vessel",
+    "Voyage",
+    "BookingNo",
+    "InsertedTimeStamp", // value manually manually
+    "UUid", // value manually manually
+    "ProcessState" // value manually manually
+];
+
+module.exports = shipmentOceanImportMapping;
