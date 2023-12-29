64b95f36714d8904506c214a469ed405bf39402d
tharun kancham <tharun.kncham@bizcloudexperts.com>
2023-12-29 14:45:59 -0600

    Updated all handler files for SNS add

--- src/realtime/shipperRt.js
+++ src/realtime/shipperRt.js
@@ -12,6 +12,9 @@ const columnsList = shipperTableMapping.concat(oprerationColumns);
 const primaryKey = "FK_ShipOrderNo";
 const sortKey = null;
 const uniqueFilterKey = "transact_id";
+const AWS = require("aws-sdk");
+const sns = new AWS.SNS({ region: "us-east-1" });
+const { SNS_TOPIC_ARN } = process.env;
 
 module.exports.handler = async (event, context, callback) => {
   let sqsEventRecords = [];
@@ -64,6 +67,11 @@ module.exports.handler = async (event, context, callback) => {
     return prepareBatchFailureObj(faildSqsItemList);
   } catch (error) {
     console.error("Error while fetching json files", error);
+    const params = {
+			Message: `Error in ${functionName}, Error: ${error.Message}`,
+			TopicArn: SNS_TOPIC_ARN,
+		};
+    await sns.publish(params).promise();
     return prepareBatchFailureObj(sqsEventRecords);
   }
 };
