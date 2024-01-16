8cd643bd88bfd81364e9db082a8dd425c877564a
tharun kancham <tharun.kncham@bizcloudexperts.com>
2024-01-16 11:42:20 -0600

    context.functionName updated in all the lambda functions.

--- src/realtime/serviceLevelRt.js
+++ src/realtime/serviceLevelRt.js
@@ -64,7 +64,7 @@ module.exports.handler = async (event, context, callback) => {
   } catch (error) {
     console.error("Error while fetching json files", error);
     const params = {
-			Message: `Error in ${functionName}, Error: ${error.Message}`,
+			Message: `Error in ${context.functionName}, Error: ${error.Message}`,
 			TopicArn: SNS_TOPIC_ARN,
 		};
     await sns.publish(params).promise();
