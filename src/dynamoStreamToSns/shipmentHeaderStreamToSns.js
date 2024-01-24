f1fecd37e178f58619241d506edf9e6074c41d2b
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-01-25 02:07:52 +0530

    AcctManager check removed

--- src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
+++ src/dynamoStreamToSns/shipmentHeaderStreamToSns.js
@@ -1,24 +1,6 @@
-const { get } = require("lodash");
 const { processDynamoDBStream } = require("../shared/dataHelper");
 
 module.exports.handler = async (event, context, callback) => {
     console.info("event", JSON.stringify(event));
-    const records = event.Records;
-    const newRecord = { ...event };
-
-    records.map(async (record, index) => {
-        const newAcctManager = get(record, "dynamodb.NewImage.AcctManager.S");
-        const oldAcctManager = get(record, "dynamodb.OldImage.AcctManager.S", null);
-
-        if (get(record, "eventName") === "MODIFY" && newAcctManager && oldAcctManager === null) {
-            console.log("AcctManager. Ignoring the event.");
-        newRecord.Records.splice(index, 1);
-            return false;
-        }
-
-        console.info("dynamodb.NewImage.AcctManager", newAcctManager);
-        console.info("dynamodb.OldImage.AcctManager", oldAcctManager);
-    });
-    console.info("🙂 -> file: shipmentHeaderStreamToSns.js:17 -> newRecord:", JSON.stringify(newRecord));
     return await processDynamoDBStream(event, process.env.SNS_TOPIC_ARN, process.env.DYNAMO_DB_TABLE, "BillNo");
 };
