ba995862e7a59f26ddc2db8656f24c5dab5f2db5
AjiteshNarra03 <ajitesh.narra@bizcloudexperts.com>
2024-06-28 02:11:00 +0530

    modified lambda

--- src/realtime/sendreport.js
+++ src/realtime/sendreport.js
@@ -1,12 +1,11 @@
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const nodemailer = require("nodemailer");
 const sesTransport = require("nodemailer-ses-transport");
 const { stringify } = require("csv-stringify");
 const moment = require("moment");
 
-const EMAIL_TO = "omnidev@bizcloudexperts.com ";
+const EMAIL_TO = "omnidev@bizcloudexperts.com";
 const EMAIL_FROM = "no-reply@omnilogistics.com";
 
 const transporter = nodemailer.createTransport(
@@ -19,35 +18,27 @@ const transporter = nodemailer.createTransport(
 
 module.exports.handler = async (event) => {
   const startDate = moment().startOf("day").format("YYYY-MM-DD");
-
-  const params = {
-    TableName: process.env.FAILED_RECORDS,
-    FilterExpression: "#Status = :FAILED ",
-    ExpressionAttributeNames: {
-      "#Status": "Status",
-    },
-    ExpressionAttributeValues: {
-      ":FAILED": "FAILED",
-    },
-  };
+  const fileName = `failed_records_${startDate}.csv`;
 
   try {
-    const data = await dynamodb.scan(params).promise();
+    const failedRecords = await fetchAllFailedRecords();
+    const failedRecordsCount = failedRecords.length;
 
-    if (data.Items.length > 0) {
+    console.log("Fetched failed records:", failedRecordsCount); // Add this line for debugging
+
+    if (failedRecords.length > 0) {
       const csvData = await new Promise((resolve, reject) => {
-        stringify(data.Items, { header: true }, (err, output) => {
+        stringify(failedRecords, { header: true }, (err, output) => {
           if (err) reject(err);
           else resolve(output);
         });
       });
-      const fileName = `failed_records_${startDate}.csv`;
 
       const emailParams = {
         from: EMAIL_FROM,
         to: EMAIL_TO,
         subject: `Failed Records Report for Realtime application ${startDate}`,
-        text: `The failed records report for ${startDate} has been successfully generated and uploaded to the S3 bucket. The file is also attached.`,
+        text: `The failed records report for ${startDate} has been successfully generated the file is also attached there are total ${failedRecordsCount} records.`,
         attachments: [
           {
             filename: fileName,
@@ -59,14 +50,51 @@ module.exports.handler = async (event) => {
 
       await transporter.sendMail(emailParams);
       console.log("Email sent successfully.");
-      return "sucess";
+      return "success";
     } else {
-      console.log("No failed records found for the previous day.");
+      console.log("No failed records found.");
+      return "no failed records";
     }
   } catch (err) {
     console.error(
-      "Error querying DynamoDB, uploading to S3, or sending email:",
+      "Error querying DynamoDB, generating CSV, or sending email:",
       err
     );
+    throw err;
   }
 };
+
+async function fetchAllFailedRecords() {
+  const params = {
+    TableName: process.env.FAILED_RECORDS,
+    IndexName: "Status-index", // Replace with the actual name of your GSI
+    KeyConditionExpression: "#Status = :FAILED",
+    ExpressionAttributeNames: {
+      "#Status": "Status",
+    },
+    ExpressionAttributeValues: {
+      ":FAILED": "FAILED",
+    },
+    Limit: 10000, // Adjust the batch size as per your needs
+  };
+
+  let allRecords = [];
+  let lastEvaluatedKey = null;
+
+  try {
+    do {
+      if (lastEvaluatedKey) {
+        params.ExclusiveStartKey = lastEvaluatedKey;
+      }
+
+      const data = await dynamodb.query(params).promise();
+      allRecords = allRecords.concat(data.Items);
+      lastEvaluatedKey = data.LastEvaluatedKey;
+    } while (lastEvaluatedKey);
+  } catch (error) {
+    console.error("Query failed:", error);
+    throw error;
+  }
+
+  return allRecords;
+}
